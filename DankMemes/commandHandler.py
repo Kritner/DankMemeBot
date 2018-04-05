42f74b72eaf8fe6311e061cc4037c8be8c88b5a4
Russ Hammett <kritner@gmail.com>
2018-04-05 12:52:44 -0400

    #3 flavortown memes

--- DankMemes/commandHandler.py
+++ DankMemes/commandHandler.py
@@ -1,20 +1,29 @@
+import random
 from slackclient import SlackClient
 
 class CommandHandler():
 
     currentTargetForDankness = "lemoneylimeslimes"
     memes = [
-                    {"trigger" : "rules", 
-                    "channel" : None, 
-                    "responses": 
-                        ["This place isn't quite dank enough to get into the rules..."]},
-                    {"trigger" : "rules", 
-                    "channel" : "dank_memers", 
-                    "responses": 
-                        [("Here are the rules..." + 
-                        "\nRule #1: Don’t tell {0}" + 
-                        "\nRule #2: If {0} still finds out, invite him." +
-                        "\nRule #3: If {0} is in this channel, he can select the next potential candidate.").format(currentTargetForDankness)]}]
+        {"trigger" : "rules", 
+        "channel" : None, 
+        "responses": 
+            ["This place isn't quite dank enough to get into the rules..."]},
+        {"trigger" : "rules", 
+        "channel" : "dank_memers", 
+        "responses": 
+            [("Here are the rules..." + 
+            "\nRule #1: Don’t tell {0}" + 
+            "\nRule #2: If {0} still finds out, invite him." +
+            "\nRule #3: If {0} is in this channel, he can select the next potential candidate.").format(currentTargetForDankness)]},
+        {"trigger" : "flavortown", 
+        "channel" : None, 
+        "responses": [
+            "http://i0.kym-cdn.com/photos/images/newsfeed/001/355/960/bf2.jpg",
+            "http://i0.kym-cdn.com/photos/images/newsfeed/001/053/453/f5f.jpg",
+            "http://i0.kym-cdn.com/photos/images/newsfeed/000/972/194/cd9.jpg",
+            "https://i.chzbgr.com/full/9101308416/h26CA5A46/"
+        ]}]
 
     def handle_command(self, slackClient, command, channel):
         """
@@ -29,9 +38,9 @@ class CommandHandler():
         for meme in self.memes:
             if command.startswith(meme["trigger"]):
                 if meme["channel"] == channel_name:
-                    response = meme["responses"][0]
+                    response = meme["responses"][random.randrange(0, len(meme["responses"]) - 1)]
                 elif meme["channel"] is None:
-                    response = meme["responses"][0]
+                    response = meme["responses"][random.randrange(0, len(meme["responses"]) - 1)]
 
         if response is None:
             return
